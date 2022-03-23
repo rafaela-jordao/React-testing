5744ca5451d5c4cb4da80d3b32d273b300e52790
Rafaela Jordão <rafa.jordao.07@hotmail.com>
2022-03-23 18:26:12 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { NotFound } from '../components';
+
+describe('Testando componente NotFound', () => {
+  test('Verifica se a página contém o texto Page request not found', () => {
+    render(<NotFound />);
+    const text = screen.getByRole('heading', {
+      name: /Page requested not found/i, level: 2 });
+    expect(text).toBeInTheDocument();
+  });
+
+  test('Verifica se a página monstra a imagem', () => {
+    render(<NotFound />);
+    const img = screen.getByRole('img', { name: /Pikachu crying/i });
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
